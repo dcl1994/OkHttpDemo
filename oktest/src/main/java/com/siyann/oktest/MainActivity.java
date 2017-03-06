6565ac04c80d8baac1ba638e9efe0fae41a4d6be
xiaolong <xiaolong@gmail.com>
2017-03-06 11:11:05 +0800

    第二次修改

--- oktest/src/main/java/com/siyann/oktest/MainActivity.java
+++ oktest/src/main/java/com/siyann/oktest/MainActivity.java
@@ -3,18 +3,30 @@ package com.siyann.oktest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private Button button;
+    private TextView mytextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button= (Button) findViewById(R.id.mybtn);
+        init();
         button.setOnClickListener(v -> {
-            Toast.makeText(MainActivity.this,"被点击了",Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "被点击了", Toast.LENGTH_SHORT).show();
         });
+
+    }
+
+    /**
+     * 初始化
+     */
+    private void init() {
+        button = (Button) findViewById(R.id.mybtn);
+        mytextview = (TextView) findViewById(R.id.mytextview);
+
     }
 }
